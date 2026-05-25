dd2f340cb14e352f998980dd965eb39c41532466
Your Name <youremail@example.com>
2026-05-26 01:02:51 +0530

    Added Prometheus metrics endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const client = require("prom-client");
+
 require("dotenv").config();
 
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +24,12 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req, res) => {
   res.send("Backend API Running");
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+
+  res.end(await client.register.metrics());
 });
 
 const PORT = process.env.PORT || 5000;
